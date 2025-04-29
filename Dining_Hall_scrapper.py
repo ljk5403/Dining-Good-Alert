74962d857904bac777052a7886d81fc55dd53a6f
JiankunLi <ljk5403@outlook.com>
2025-04-28 21:47:46 -0500

    Add dishes

--- Dining_Hall_scrapper.py
+++ Dining_Hall_scrapper.py
@@ -16,8 +16,8 @@ dinning_hall_tuple = ('rhetas-market', 'lizs-market', 'gordon-avenue-market', 'f
 #meals_tuple = ('breakfast', 'lunch', 'dinner')
 meals_tuple = ('lunch', 'dinner')
 
-good_dish_category = ['clam', 'Shrimp', 'Cod', 'squid', 'octopus', 'Tuna', 'Salmon', 'catfish', 'Tilapia', 'fish', 'lamb', 'goat', 'curry', 'beef', 'pork', 'chicken']
-good_dish_name_T0 =['Moroccan Style Lamb Stuffed Peppers', 'lobster', 'crayfish', 'watermelon', 'Drunken Noodles', 'Beef Alicha', 'Pepper, Onion, & Mushroom Saute', 'Poke Bar', 'corn bake', 'drumstick', 'Chicken Drumstick Gochujang Glazed', 'Chicken Drummies', 'Tandoori Chicken', 'Samosas', 'steak']
+good_dish_category = ['clam', 'Shrimp', 'Cod', 'squid', 'octopus', 'Salmon', 'mahi mahi', 'catfish', 'Tilapia', 'Tuna', 'fish', 'lamb', 'goat', 'curry', 'beef', 'pork', 'chicken']
+good_dish_name_T0 =['Moroccan Style Lamb Stuffed Peppers', 'lobster', 'crayfish', 'watermelon', 'Drunken Noodles', 'Beef Alicha', 'Pepper, Onion, & Mushroom Saute', 'Poke Bar', 'corn bake', 'Rapini', 'broccoli rabe', 'drumstick', 'Chicken Drumstick Gochujang Glazed', 'Chicken Drummies', 'Tandoori Chicken', 'Samosas', 'Pakoras', 'steak']
 good_dish_name_T1 =['BBQ Pork Sandwich']
 
 good_dish_list = good_dish_name_T0 + good_dish_name_T1 + good_dish_category
